eed780260fae41974ef570119c8b6ac05787d953
bmcc-mmp <52509720+bmcc-mmp@users.noreply.github.com>
2019-07-06 13:56:48 -0400

    Update footer.php

--- wordpress/MMP460-minimal-theme/footer.php
+++ wordpress/MMP460-minimal-theme/footer.php
@@ -1,9 +1,12 @@
-    <footer><small>
-       &#169; Copyright <?php the_date(Y);?> <?php bloginfo('name');?> | Email: <a href="mailto:<?php the_author_email(); ?> "> <?php the_author_email(); ?> </a></small>
-    </footer>
-    </div>
-    <?php
-	wp_footer();
-?>
-  </body>
+			<footer>
+				<!-- You can write anything that you like here. This code grabs the year, site name and the administrator email that are entred in the site's dashboard. -->
+				<small>
+					&#169; Copyright <?php the_date(Y);?> <?php bloginfo('name');?> | Email: <a href="mailto:<?php 						the_author_email(); ?> "> <?php the_author_email(); ?> </a>
+				</small>
+    			</footer>
+		</div> <!-- closes the wrapper div that was opened in header.php -->
+   
+<?php wp_footer();?>
+
+	</body>
 </html>
