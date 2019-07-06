e657c1ea60c41fd8ddec8321cc2ac0a1074fadbf
bmcc-mmp <52509720+bmcc-mmp@users.noreply.github.com>
2019-07-06 13:57:20 -0400

    Update footer.php

--- wordpress/MMP460-minimal-theme/footer.php
+++ wordpress/MMP460-minimal-theme/footer.php
@@ -1,5 +1,5 @@
 			<footer>
-				<!-- You can write anything that you like here. This code grabs the year, site name and the administrator email that are entred in the site's dashboard. -->
+				<!-- You can write anything that you like here. This code grabs the year, site name and the administrator's email that were entered in the site's dashboard. -->
 				<small>
 					&#169; Copyright <?php the_date(Y);?> <?php bloginfo('name');?> | Email: <a href="mailto:<?php 						the_author_email(); ?> "> <?php the_author_email(); ?> </a>
 				</small>
